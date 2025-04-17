7d9bce848d41f669eb7cf8975b60b27a65042bd1
Jarrett Phillips <jphill01@uoguelph.ca>
2025-04-17 09:42:23 -0400

    Add files via upload

--- DPM Sensitivity Analysis.R
+++ DPM Sensitivity Analysis.R
@@ -9,18 +9,28 @@
 library(yaml)
 library(rstudioapi)
 
-file_path <- selectDirectory()
+file_path <- selectDirectory() # navigate to directory
+base_yaml_path <- file.choose() # select file to modify
 
-base_yaml_path <- file.choose()
 yaml_data <- yaml.load_file(base_yaml_path)
 
-param <- "part"
+param_steps <- list(
+  part = seq(from = 0, to = 1.1, by = 0.1),
+  prolif = seq(from = 0, to = 2, by = 0.1)
+  # add more parameters as needed
+)
 
-steps <- seq(from = 0, to = 1, by = 0.1)
-
-for (val in steps) {
-  yaml_data[[param]] <- val  
-  filename <- sprintf("%s_%.1f.yaml", param, val)  
-  full_path <- file.path(file_path, filename)
-  writeLines(as.yaml(yaml_data), full_path)
+for (param in names(param_steps)) {
+  steps <- param_steps[[param]]
+  
+  for (val in steps) {
+    mod_yaml <- yaml_data         
+    mod_yaml[[param]] <- val      
+    
+    filename <- sprintf("%s_%.1f.yaml", param, val)
+    full_path <- file.path(file_path, filename)
+    
+    writeLines(as.yaml(mod_yaml), full_path)
+  }
 }
+
